15587f360d9cc446146ca427cdb83e8355318836
Tushar Maurya <tusharmaurya43@gmail.com>
2024-06-16 02:35:25 +0530

    Leaderboard route fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Footer from './components/Footer';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Leaderboard from './pages/Leaderboard';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/leaderboard' element={<Leaderboard/>}/>
       </Routes>
       <Footer/>
     </Router>
